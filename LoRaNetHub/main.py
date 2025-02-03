a3f0f797aa35ad575600fa238f165f4720b2be85
Jonathan Eisenbrandt <jeisenbrandt@volleyboast.com>
2025-02-03 11:04:47 -0600

    commiting working state changes for insert data

--- LoRaNetHub/main.py
+++ LoRaNetHub/main.py
@@ -4,36 +4,73 @@ import paho.mqtt.client as mqtt
 import threading
 import json
 import base64
+import time
+import psycopg2
+from psycopg2.extras import execute_values
+import pytz
+from datetime import datetime
 
+conn = psycopg2.connect(
+    dbname="loranethubdb",
+    user="postgres",
+    password="volleyboast",
+    host="localhost",
+    port="5432"
+)
+cur = conn.cursor()
 mqttClient = mqtt.Client()
-
 
 def mqttOnConnect(client, userdata, flags, rc):
     print("MQTT Connected with result code "+str(rc))
 
-    # mqttClient.subscribe("lora/" + "00-80-00-00-00-01-71-31" + "/down_queued")
-    # mqttClient.subscribe("lora/" + "00-80-00-00-00-01-71-31" + "/down_dropped")
-    # mqttClient.subscribe("lora/" + "00-80-00-00-00-01-71-31" + "/queue_full")
-    # mqttClient.subscribe("lora/" + "00-80-00-00-00-01-71-31" + "/cleared")
-    # mqttClient.subscribe("lora/{}/up".format(deveui))
-    #subscribe to all packets
-    mqttClient.subscribe("gateway/+/rx")
+    mqttClient.subscribe("lora/#")
 
 def mqttOnPublish(client, userdata, result):
     print("MQTT Data published \n")
 
 def mqttOnMessage(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-    # if msg.topic == "lora/{}/up".format(deveui):
-    #     processPayload(msg)
-    # if msg.topic == "lora/{}/down_queued".format(deveui):
-    #     global counter
-    #     counter = counter + 1
-    #     print("Downlink queued {}".format(counter))
-    # elif msg.topic == "lora/{}/down_dropped".format(deveui):
-    #     print("Downlink dropped")
-    # else:
-    #     print(msg.topic+" "+str(msg.payload))
+    #store packet
+    data_to_insert = {}
+    if "up" in msg.topic: # or "packet_recv" in msg.topic:
+        decoded_payload = msg.payload.decode('utf-8')
+        msg_dict = json.loads(decoded_payload)
+        data_to_insert = (
+            msg_dict["jver"],
+            msg_dict["tmst"],
+            msg_dict["chan"],
+            msg_dict["rfch"],
+            msg_dict["freq"],
+            msg_dict["mid"],
+            msg_dict["stat"],
+            msg_dict["modu"],
+            msg_dict["datr"],
+            msg_dict["codr"],
+            msg_dict["rssis"],
+            msg_dict["lsnr"],
+            msg_dict["foff"],
+            msg_dict["rssi"],
+            msg_dict["opts"],
+            msg_dict["size"],
+            msg_dict["fcnt"],
+            msg_dict["cls"],
+            msg_dict["port"],
+            msg_dict["mhdr"],
+            msg_dict["data"],
+            msg_dict["appeui"],
+            msg_dict["deveui"],
+            msg_dict["devaddr"],
+            msg_dict["ack"],
+            msg_dict["adr"],
+            msg_dict["gweui"],
+            msg_dict["seqn"],
+            msg_dict["time"],
+            "UpUnc"
+        )
+    elif "packet_recv" in msg.topic:
+        #implement later
+        print("packet recieved")
+    if data_to_insert != {}:
+        insert_data( data_to_insert)
 
 def mqttLoopThread():
     while True:
@@ -63,37 +100,39 @@ def mqttPublish(deveui, downlinkPayload):
     else:
         return False
 
-async def create_connection_pool():
-    return await asyncpg.create_pool(dsn="postgresql://postgres:volleyboast@localhost/loranethubdb")
+def insert_data(data):
+    insert_query = """
+        INSERT INTO packets(
+                                
+    jver, tmst, chan, rfch, freq, mid, stat, modu, datr, codr, rssis, lsnr,
+    foff, rssi, opts, size, fcnt, cls, port, mhdr, data, appeui, deveui, 
+    devaddr, ack, adr, gweui, seqn, time, type
+    )
+    VALUES %s
+    """
+    print("inserting data")
+    timezone = pytz.timezone('UTC')
+    timestamp = timezone.localize(datetime(2025, 2, 3, 14, 30, 0))
+    data = [
+    (1, 123456789, 1, 2, 915, 123, 1, 'FSK', 'SF12', '4/5', 10, 7, 500, 30, None, 64, 1, 10, 1, 'abcdef', 'A001122', 'D001122', '01ABC123', 'temp', False, True, 'GWEU123', 100, None, 'Join')
+    ]
+    execute_values(cur, insert_query, data)
 
-async def insert_data(pool, data):
-    async with pool.acquire() as connection:
-        await connection.execute('''
-            INSERT INTO my_table(name, age) VALUES($1, $2)
-        ''', data['name'], data['age'])
+# def fetch_data(pool):
+#     async with pool.acquire() as connection:
+#         return await connection.fetch('SELECT * FROM my_table')
 
-async def fetch_data(pool):
-    async with pool.acquire() as connection:
-        return await connection.fetch('SELECT * FROM my_table')
-
-async def main():
-    pool = await create_connection_pool()
-
+def main():
     #will have to do this for each gateway
     mqttConnect("10.1.10.31")
+    #need to keep the main thread running so that the app doesn't stop
+    while True:
+        time.sleep(5)
 
-    # Example to insert data concurrently
-    # tasks = [
-    #     insert_data(pool, {'name': 'Alice', 'age': 30}),
-    #     insert_data(pool, {'name': 'Bob', 'age': 25}),
-    # ]
-    # await asyncio.gather(*tasks)
-
-    # Example to fetch data concurrently
-    # data = await fetch_data(pool)
-    # print(data)
-
-    await pool.close()
+    if cur:
+        cur.close()
+    if conn:
+        conn.close()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
